f4450c9793ae14dc45ed4171ab7a0f12de1b4c77
dannybeales <s185384@ucs.ac.uk>
2016-11-25 00:50:43 +0000

    Linked Lists - Testing Added
    Testing InsertBeginning and After not remove yet.

---
+++ LinkedList/LinkedListV2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+
+            list.InsertBeginning(3);
+            list.InsertAfter(5, 6);
+            list.InsertBeginning(5);
+
+        }
+    }
+}
---
+++ LinkedList/LinkedListV2/Testing.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace LinkList
+{
+    [TestFixture]
+    class Testing
+    {
+        [Test]
+        public void InsertBeginning()
+        {
+            LinkedList list = new LinkedList();
+
+            list.InsertBeginning(1);
+            Assert.That(list.Length(), Is.EqualTo(1));
+
+        }
+		[Test]
+		public void InsertAfter()
+		{
+			LinkedList list = new LinkedList();
+			list.InsertAfter(1);
+			Assert.That(list.Length(), Is.EqualTo(2));
+			
+		}
+
+
+
+
+    }
+}
